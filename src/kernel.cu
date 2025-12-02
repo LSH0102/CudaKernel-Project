9afb1cfc7229fee52a884aacb4c07d0308ae8939
LSH0102 <farewell2006@qq.com>
2025-12-02 15:55:51 +0800

    submit bmm

--- src/kernel.cu
+++ src/kernel.cu
@@ -2,6 +2,7 @@
 #include <cstddef>
 #include <stdio.h>
 
+const int TILE_SIZE=128;
 ///用于测试用的向量加法内核
 __global__ void vector_add_kernel(float* a, float* b,float*c,const int size)
 {
@@ -11,6 +12,35 @@ __global__ void vector_add_kernel(float* a, float* b,float*c,const int size)
 		c[id]=a[id]+b[id];
 	}
 	return;
+}
+
+/// 批量矩阵乘法内核
+__global__ void bmm_kernel(float *a,float* b,float *c,const int batch_size, const int m, const int n, const int p)
+{
+	//每个block处理一对矩阵
+	int batch_id=blockIdx.x;
+
+	//每个thread处理一个 TILE_SIZE x TILE_SIZE 的小矩阵块 i.e左上角为 TILE_SIZE *i,TILE_SIZE *j 的矩阵块
+	int i=threadIdx.x;
+	int j=threadIdx.y;
+	int k=0;
+	int row=0;
+	int column=0;
+	float temp=0.0;
+
+	//计算c中第row行, column列的结果
+	for(row=i*TILE_SIZE;row<m&& row<(i+1)*TILE_SIZE;row++)
+	{
+		for(column=j*TILE_SIZE;column<p&&column<(j+1)*TILE_SIZE;column++)
+		{
+			temp=0.0;
+			for(k=0;k<n;k++)
+			{
+				temp+=a[batch_id*m*n+n*row+k]*b[batch_id*n*p+p*k+column];
+			}
+			c[batch_id*m*p+p*row+column]=temp;
+		}
+	}
 }
 
 ///调用内核的主程序
@@ -43,7 +73,38 @@ extern "C"
 		cudaFree(d_a);
 		cudaFree(d_b);
 		cudaFree(d_c);
+	}
 
+	void launch_bmm(float *a,float* b,float *c,const int batch_size, const int m, const int n, const int p,cudaStream_t stream )
+	{
+		int a_size=sizeof(float)*m*n*batch_size;
+		int b_size=sizeof(float)*p*n*batch_size;
+		int c_size=sizeof(float)*m*p*batch_size;
 
+		float * d_a, *d_b,*d_c;
+		cudaMalloc((void**)&d_a,a_size);
+		cudaMalloc((void**)&d_b,b_size);
+		cudaMalloc((void**)&d_c,c_size);
+
+		cudaMemcpy(d_a,a,a_size,cudaMemcpyHostToDevice);
+		cudaMemcpy(d_b,b,b_size,cudaMemcpyHostToDevice);
+		cudaMemcpy(d_c,c,c_size,cudaMemcpyHostToDevice);
+
+		//支持的最大长与宽为32*TILE_SIZE
+		int nthreadx=(m+TILE_SIZE-1)/TILE_SIZE;
+		int nthready=(p+TILE_SIZE-1)/TILE_SIZE;
+		
+		dim3 grid_dim(batch_size);
+		dim3 block_dim(nthreadx,nthready);
+
+		bmm_kernel<<<grid_dim,block_dim,0,stream>>>(d_a,d_b,d_c, batch_size,m,n,p);
+
+		cudaMemcpy(c,d_c,c_size,cudaMemcpyDeviceToHost);
+
+		cudaDeviceSynchronize();
+
+		cudaFree(d_a);
+		cudaFree(d_b);
+		cudaFree(d_c);
 	}
 }
--- test.py
+++ test.py
@@ -7,7 +7,8 @@ Created on Mon Dec  1 18:49:00 2025
 import ctypes
 import torch
 import numpy as np
-lib_add=ctypes.CDLL("cuda_kernels/kernel.so")
+import time 
+lib=ctypes.CDLL("cuda_kernels/kernel.so")
 datatype=np.float32
 
 def vector_add(a:torch.Tensor,b:torch.Tensor):
@@ -17,7 +18,7 @@ def vector_add(a:torch.Tensor,b:torch.Tensor):
     stream=torch.cuda.current_stream().cuda_stream
     
     
-    lib_add.launch_add.argtypes=[
+    lib.launch_add.argtypes=[
         
         np.ctypeslib.ndpointer(dtype=datatype,ndim=1,flags='C_CONTIGUOUS'),
         np.ctypeslib.ndpointer(dtype=datatype,ndim=1,flags='C_CONTIGUOUS'),
@@ -25,19 +26,44 @@ def vector_add(a:torch.Tensor,b:torch.Tensor):
         ctypes.c_int,
         ctypes.c_void_p]
     
-    lib_add.launch_add.restype=None
+    lib.launch_add.restype=None
     
     res=c.view(-1).cpu().numpy()
-    lib_add.launch_add(a.view(-1).cpu().numpy(),b.view(-1).cpu().numpy(),res,size,stream)
+    lib.launch_add(a.view(-1).cpu().numpy(),b.view(-1).cpu().numpy(),res,size,stream)
     torch.cuda.synchronize()
     
     c=torch.from_numpy(res).to(a.device)
     c=c.reshape(shape)
     return c
 
-
-if __name__=="__main__":
+def bmm(a:torch.Tensor,b:torch.Tensor):
+    batch_size,m,n=a.shape
+    p=b.shape[-1]
     
+    res=torch.zeros(size=(batch_size,m,p)).view(-1).numpy()
+    stream=torch.cuda.current_stream().cuda_stream
+    
+    lib.launch_bmm.argtypes=[
+        
+        np.ctypeslib.ndpointer(dtype=datatype,ndim=1,flags='C_CONTIGUOUS'),
+        np.ctypeslib.ndpointer(dtype=datatype,ndim=1,flags='C_CONTIGUOUS'),
+        np.ctypeslib.ndpointer(dtype=datatype,ndim=1,flags='C_CONTIGUOUS'),
+        ctypes.c_int,
+        ctypes.c_int,
+        ctypes.c_int,
+        ctypes.c_int,
+        ctypes.c_void_p]
+    lib.launch_bmm.restype=None
+    
+    lib.launch_bmm(a.view(-1).cpu().numpy(),b.view(-1).cpu().numpy(),res, batch_size,m,n,p,stream)
+    c=torch.from_numpy(res).to(a.device)
+    c=c.reshape((batch_size,m,p))
+    
+    return c
+    
+    
+
+def test_vector_add():
     shape=(2,3,4) 
     
     a=torch.randn(shape)
@@ -48,5 +74,59 @@ if __name__=="__main__":
     b=b.cuda()
     c=vector_add(a, b)
     print(c)
+
+def test_bmm_1():
+    batch_size=1
+    m,n,p=66,129,256
+    
+    shape_a=(batch_size,m,n)
+    shape_b=(batch_size,n,p)
+    
+    a=torch.randn(shape_a)
+    b=torch.randn(shape_b)
+    
+    ref=torch.bmm(a,b).numpy()
+    
+    a=a.cuda()
+    b=b.cuda()
+    
+    c=bmm(a, b)
+    
+    c=c.cpu().numpy()
+    
+    assert np.abs(c-ref).max()<1e-3
+    
+
+def test_bmm():
+    for i in range(0,10):
+        batch_size=np.random.randint(64,128)
+        m,n,p=np.random.randint(512,1024,size=(3,))
+        
+        shape_a=(batch_size,m,n)
+        shape_b=(batch_size,n,p)
+        
+        a=torch.randn(shape_a)
+        b=torch.randn(shape_b)
+        
+        ref=torch.bmm(a,b).numpy()
+        
+        a=a.cuda()
+        b=b.cuda()
+        
+        c=bmm(a, b)
+        
+        c=c.cpu().numpy()
+        
+        
+        assert np.abs(c-ref).max()<1e-3
+        info='test {num} passed, for shape {shape1} and {shape2}.\n The maximal absolute error is {e}.'.format(num=i,shape1=tuple(a.shape),
+                                                                                                             shape2=tuple(b.shape),
+                                                                                                               e=np.abs(c-ref).max())
+        print(info)
+    
+
+if __name__=="__main__":
+    
+    test_bmm()
     
     

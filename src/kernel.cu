0a7200ac70069205e0fbe4657b5a8eecaeb0dc5b
LSH0102 <farewell2006@qq.com>
2025-12-13 19:54:42 +0800

    submit conv2d

--- src/kernel.cu
+++ src/kernel.cu
@@ -2,7 +2,7 @@
 #include <cstddef>
 #include <stdio.h>
 
-const int TILE_SIZE=128;
+const int TILE_SIZE=64;
 ///用于测试用的向量加法内核
 __global__ void vector_add_kernel(float* a, float* b,float*c,const int size)
 {
@@ -39,6 +39,61 @@ __global__ void bmm_kernel(float *a,float* b,float *c,const int batch_size, cons
 				temp+=a[batch_id*m*n+n*row+k]*b[batch_id*n*p+p*k+column];
 			}
 			c[batch_id*m*p+p*row+column]=temp;
+		}
+	}
+}
+
+///shift函数内核, 用于读取 X[:,i:i+width*stride:stride,j:j+height*stride:stride,:]
+__global__ void shift_kernel(float* x, float* res, int i,int j,int stride, const int input_H, const int input_W, const int out_H, const int out_W,const int in_channel)
+{
+	//每个block处理一个矩阵
+	int batch_id=blockIdx.x;
+	//每个thread处理一张图片的若干部分
+	
+	int segment_x=(out_H+32)/32;
+	int segment_y=(out_W+32)/32;
+	int m,n,p;
+	for(m=0;m<segment_x&&threadIdx.x*segment_x+m<out_H;m++)
+	{
+		for(n=0;n<segment_y&&threadIdx.y*segment_y+n<out_W;n++)
+		{
+			//准备处理 (threadIdx.x*segment_x+m, threadIdx.y*segment_y+n)的数据
+			for(p=0;p<in_channel;p++)
+			{
+				res[batch_id*out_H*out_W*in_channel+(threadIdx.x*segment_x+m)*out_W*in_channel+(threadIdx.y*segment_y+n)*in_channel+p]=x[batch_id*input_H*input_W*in_channel+(i+stride*(threadIdx.x*segment_x+m))*input_W*in_channel+(j+stride*(threadIdx.y*segment_y+n))*in_channel+p];
+			}
+		}
+	}
+
+
+
+}
+
+///conv2d的特殊bmm 此时可以把矩阵b看成每个batch都相同的矩阵, 即batch_id永远等于0
+__global__ void conv_bmm(float *shift, float* filter, float *c,int batch_size,const int m,const int n, const int p)
+{
+	//每个block处理一对矩阵
+	int batch_id=blockIdx.x;
+
+	//每个thread处理一个 TILE_SIZE x TILE_SIZE 的小矩阵块 i.e左上角为 TILE_SIZE *i,TILE_SIZE *j 的矩阵块
+	int i=threadIdx.x;
+	int j=threadIdx.y;
+	int k=0;
+	int row=0;
+	int column=0;
+	float temp=0.0;
+
+	//计算c中第row行, column列的结果
+	for(row=i*TILE_SIZE;row<m&& row<(i+1)*TILE_SIZE;row++)
+	{
+		for(column=j*TILE_SIZE;column<p&&column<(j+1)*TILE_SIZE;column++)
+		{
+			temp=0.0;
+			for(k=0;k<n;k++)
+			{
+				temp+=shift[batch_id*m*n+n*row+k]*filter[p*k+column];
+			}
+			c[batch_id*m*p+p*row+column]+=temp;
 		}
 	}
 }
@@ -106,5 +161,85 @@ extern "C"
 		cudaFree(d_a);
 		cudaFree(d_b);
 		cudaFree(d_c);
+	}
+
+	void launch_shift(float* x, float* res, int i,int j,int batch_size,int stride,const int input_H, 
+							const int input_W, const int out_H, const int out_W,const int in_channel,cudaStream_t stream)
+	{
+		int input_size=sizeof(float)*input_H*input_W*in_channel*batch_size;
+		int out_size=sizeof(float)*out_H*out_W*in_channel*batch_size;
+
+		float *d_in,*d_res;
+		cudaMalloc((void**)&d_in,input_size);
+		cudaMalloc((void**)&d_res,out_size);
+
+		cudaMemcpy(d_in,x,input_size,cudaMemcpyHostToDevice);
+
+		dim3 grid_dim(batch_size);
+		dim3 block_dim(32,32,1);
+
+		shift_kernel<<<grid_dim,block_dim,0,stream>>>(d_in,d_res,i,j,stride,input_H,input_W,out_H,out_W,in_channel);
+
+		cudaMemcpy(res,d_res,out_size,cudaMemcpyDeviceToHost);
+
+		cudaFree(d_in);
+		cudaFree(d_res);
+
+	}
+
+	void launch_conv2d(float *input, float *filter, float * out,const int batch_size, const int stride, const int input_H, const int input_W, 
+						const int filter_H, const int filter_W, 
+						const int out_H,const int out_W,
+						const int in_channel, const int out_channel,cudaStream_t stream)
+	{
+		int input_size=sizeof(float)*input_H*input_W*in_channel*batch_size;
+		int filter_size=sizeof(float)*filter_H*filter_W*out_channel*in_channel;
+		int out_size=sizeof(float)*out_H*out_W*out_channel*batch_size;
+		int shift_size=sizeof(float)*batch_size*in_channel*input_H*input_W;
+
+		float *d_in, *d_filter, *d_out, *temp, *shift;
+		cudaMalloc((void**)&d_in,input_size);
+		cudaMalloc((void**)&d_filter,filter_size);
+		cudaMalloc((void**)&d_out,out_size);
+		cudaMalloc((void**)&temp,out_size);
+
+		cudaMalloc((void**)&shift,shift_size);
+
+
+		cudaMemcpy(d_in,input,input_size,cudaMemcpyHostToDevice);
+		cudaMemcpy(d_filter,filter,filter_size,cudaMemcpyHostToDevice);
+		cudaMemcpy(d_out,out,out_size,cudaMemcpyHostToDevice);
+		cudaMemcpy(temp,out,out_size,cudaMemcpyHostToDevice);
+
+		dim3 grid_dim(batch_size);
+		dim3 block_dim(32,32,1);
+
+		int nthreadx=(out_H*out_W+TILE_SIZE-1)/TILE_SIZE;
+		int nthready=(out_channel+TILE_SIZE-1)/TILE_SIZE;
+		dim3 block_dim2(nthreadx,nthready);
+
+
+		int i,j;
+		for(i=0;i<filter_H;i++)
+		{
+			for(j=0;j<filter_W;j++)
+			{
+				//读取input的一部分
+				shift_kernel<<<grid_dim,block_dim,0,stream>>>(d_in,shift,i,j,stride,input_H,input_W,out_H,out_W,in_channel);
+				//和filter的[i,j]个元素做bmm, 并加给d_out
+				conv_bmm<<<grid_dim,block_dim2,0,stream>>>(shift,d_filter+i*filter_W*in_channel*out_channel+j*in_channel*out_channel,d_out,batch_size,out_H*out_W,in_channel,out_channel);
+			}
+
+		}
+		
+		cudaMemcpy(out,d_out,out_size,cudaMemcpyDeviceToHost);
+
+		cudaFree(d_in);
+		cudaFree(d_filter);
+		cudaFree(d_out);
+		cudaFree(temp);
+		cudaFree(shift);
+
+
 	}
 }
--- test.py
+++ test.py
@@ -60,8 +60,75 @@ def bmm(a:torch.Tensor,b:torch.Tensor):
     c=c.reshape((batch_size,m,p))
     
     return c
+
+def conv2d(a:np.ndarray,w:np.ndarray,stride,padding):
+    
+    batch_size=a.shape[0]
+    in_channel=a.shape[-1]
+    width=a.shape[2]
+    height=a.shape[1]
+    out_channel=w.shape[-1]
+    
+    o_height=(2*padding+height-w.shape[0])//stride+1
+    o_width=(2*padding+width-w.shape[1])//stride+1
+    
+    a=np.pad(a, pad_width=((0,0),(padding,padding),(padding,padding),(0,0)))
+    
+    res=torch.zeros(size=(batch_size,o_height,o_width,out_channel)).view(-1).numpy()
+    stream=torch.cuda.current_stream().cuda_stream
+    
+    lib.launch_conv2d.argtypes=[
+        np.ctypeslib.ndpointer(dtype=datatype,ndim=1,flags='C_CONTIGUOUS'),
+        np.ctypeslib.ndpointer(dtype=datatype,ndim=1,flags='C_CONTIGUOUS'),
+        np.ctypeslib.ndpointer(dtype=datatype,ndim=1,flags='C_CONTIGUOUS'),
+        ctypes.c_int,
+        ctypes.c_int,
+        ctypes.c_int,
+        ctypes.c_int,
+        ctypes.c_int,
+        ctypes.c_int,
+        ctypes.c_int,
+        ctypes.c_int,
+        ctypes.c_int,
+        ctypes.c_int,
+        ctypes.c_void_p]
+    
+    lib.launch_conv2d.restype=None
+    
+    lib.launch_conv2d(a.flatten(),w.flatten(),res,batch_size,stride,height+2*padding,width+2*padding,w.shape[0],w.shape[1],o_height,o_width,in_channel,out_channel,stream)
+    
+    res=res.reshape((batch_size,o_height,o_width,out_channel))
+    return res
     
     
+
+def shift(a:np.ndarray,i,j,o_height,o_width,stride):
+    b,h,w,c=a.shape
+    res=torch.zeros(size=(b,o_height,o_width,c)).view(-1).numpy()
+    
+    stream=torch.cuda.current_stream().cuda_stream
+    
+    lib.launch_shift.argtypes=[
+        
+        np.ctypeslib.ndpointer(dtype=datatype,ndim=1,flags='C_CONTIGUOUS'),
+        np.ctypeslib.ndpointer(dtype=datatype,ndim=1,flags='C_CONTIGUOUS'),
+        ctypes.c_int,
+        ctypes.c_int,
+        ctypes.c_int,
+        ctypes.c_int,
+        ctypes.c_int,
+        ctypes.c_int,
+        ctypes.c_int,
+        ctypes.c_int,
+        ctypes.c_int,
+        ctypes.c_void_p]
+    
+    lib.launch_shift.restype=None
+    
+    
+    lib.launch_shift(a.flatten(),res,i,j,b,stride,h,w,o_height,o_width,c,stream)
+    res=res.reshape((b,o_height,o_width,c))
+    return res
 
 def test_vector_add():
     shape=(2,3,4) 
@@ -95,7 +162,9 @@ def test_bmm_1():
     c=c.cpu().numpy()
     
     assert np.abs(c-ref).max()<1e-3
-    
+
+
+
 
 def test_bmm():
     for i in range(0,10):
@@ -124,9 +193,74 @@ def test_bmm():
                                                                                                                e=np.abs(c-ref).max())
         print(info)
     
+def test_shift():
+    x=torch.randn(size=(4,256,256,3)).numpy()
+    w=torch.randn(size=(18,18,3,64)).numpy()
+    stride=6
+    padding=5
+    
+    batch_size=x.shape[0]
+    in_channel=x.shape[-1]
+    width=x.shape[2]
+    height=x.shape[1]
+    out_channel=w.shape[-1]
+    
+    o_height=(2*padding+height-w.shape[0])//stride+1
+    o_width=(2*padding+width-w.shape[1])//stride+1
+    
+    x=np.pad(x, pad_width=((0,0),(padding,padding),(padding,padding),(0,0)))
+    
+    for i in range(0,w.shape[0]):
+        for j in range(0,w.shape[1]):
+            ref=x[:,i:i+o_height*stride:stride,j:j+o_width*stride:stride,:]
+            out=shift(x, i, j, o_height, o_width, stride)
+            assert np.abs(out-ref).max()<1e-3
+
+def conv_np(x,w,stride,padding):
+    
+
+    batch_size=x.shape[0]
+    in_channel=x.shape[-1]
+    width=x.shape[2]
+    height=x.shape[1]
+    out_channel=w.shape[-1]
+    
+    o_height=(2*padding+height-w.shape[0])//stride+1
+    o_width=(2*padding+width-w.shape[1])//stride+1
+    
+    out=np.zeros((batch_size,o_height,o_width,out_channel))
+    
+    x=np.pad(x, pad_width=((0,0),(padding,padding),(padding,padding),(0,0)))
+    
+    for i in range(0,w.shape[0]):
+        for j in range(0,w.shape[1]):
+            shift=x[:,i:i+o_height*stride:stride,j:j+o_width*stride:stride,:]
+            shift=shift.reshape((batch_size,o_width*o_height,in_channel))
+            filt=w[i,j].reshape((1,in_channel,out_channel))
+          
+            out+=np.matmul(shift,filt).reshape((batch_size,o_height,o_width,out_channel))
+    
+    return out            
+
+def test_conv2d():
+    pad=6
+    st=5
+    x=torch.randn(size=(5,256,256,3)).numpy()
+    w=torch.randn(size=(32,32,3,64)).numpy()
+    
+    t1=time.time()
+    out=conv2d(x, w, stride=st, padding=pad)  
+    t2=time.time()
+    ref=conv_np(x, w, stride=st, padding=pad)
+    t3=time.time()
+    
+    acc=(t3-t2)/(t2-t1)
+    print('maximal error:',np.abs(out-ref).max())
+    print('acceleration:',acc)
+    assert np.abs(out-ref).max()<1e-3
+
 
 if __name__=="__main__":
-    
-    test_bmm()
+    test_conv2d()
     
     
